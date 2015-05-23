06285131dd6a6836371653cf2b011dc52a14dda2
Kaspars Mickēvičs <kaspars@fx.lv>
2015-05-23 17:26:46 +0300

    Listen port is a variable, OS module imported.

--- helloworld.js
+++ helloworld.js
@@ -1,5 +1,8 @@
 var http = require('http');
+var os = require("os");
 var get_ip = require('ipware')().get_ip;
+
+var http_port = 3000;
 
 var server = http.createServer(function(req, res) {
     var ip_info = get_ip(req);
@@ -10,5 +13,6 @@ var server = http.createServer(function(req, res) {
     res.end();
 });
 
-server.listen(3000);
-
+server.listen(http_port);
+console.log("Running on",os.hostname());
+console.log("now listening on port", http_port);
